41e247590e0067f99515b3dca6c0c79e814f4980
SofiaaCruz <sofiaacruz187@gmail.com>
2023-06-02 19:17:01 -0300

    Ejercicio01

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using EspacioCalculadora;
+
+Calculadora calculadora = new Calculadora();
+
+int op;
+double num;
+
+
+
+Console.Write("\n\nIngrese un número: ");
+            
+while(!double.TryParse(Console.ReadLine(), out num)){
+    Console.Write("\n\nDebe ingresar número\n>> ");
+}
+
+do{
+    Console.Write("\n\nElija unaoperación:\n1-SUMAR\n2-RESTAR\n3-MULTIPLICAR\n4-DIVIDIR\n0-NADA\n>> ");
+    while(!int.TryParse(Console.ReadLine(), out op) ||op<0 || op > 4){
+        Console.Write("\n\nDebe ingresar una opcionvalida\n>> ");
+    }
+    
+    if(op!=0){
+        switch(op){
+            case 1: calculadora.Sumar(num);
+                Console.Write($"\n\nEl resultadoes: {calculadora.resultado}");
+            break;
+            case 2: calculadora.Restar(num);
+                Console.Write($"\n\nEl resultado es:{calculadora.resultado}");
+            break;
+            case 3:calculadora.Multiplicar(num);
+                Console.Write($"\n\nEl resultado es:{calculadora.resultado}");
+            break;
+            case 4:calculadora.Dividir(num);
+                Console.Write($"\n\nEl resultado es:{calculadora.resultado}");
+            break;
+        }
+    }
+    else{
+        calculadora.Limpiar();
+    }
+}while(op!=0);
---
+++ README.md
@@ -0,0 +1,5 @@
+# Parámetro --output
+
+El uso de este parámetro permite elegir la ubicación donde se generará el resultado de un comando. Si se utiliza "--output ." el resultado se generará en el directorio actual en el se está trabajando. Lo mismo sucede si se usa el parámetro sin especificar una ruta.
+
+Llamar al comando "donet new console --output ." significa que se creará un nuevo proyecto de consola en el directorio actual y la salida del resultado del comando se generará en el mismo directorio.
